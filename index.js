b5bf5330cf850b42c767dac347c15f11a07e731e
teja-dev-tech <135051682+teja-dev-tech@users.noreply.github.com>
2023-10-03 22:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,13 +53,17 @@ const Reg = mongoose.model("Reg", regSchema);
 
 app.get("/", function (req, res) {
   // Find all items in the Posts collection and render it into our home page.
+  try{
   Post.find().then((posts) => {
     res.render("home", {
       startingContent: homeStartingContent,
       posts: posts,
       message: "",
     });
-  });
+  });}
+  catch (err) {
+    next(err);
+  }
 });
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
